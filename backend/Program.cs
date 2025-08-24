f6c92f650b8ab0ffdea5aa8fd817db514d5ee72b
10GO-dev <Diegoperalta0929@gmail.com>
2025-08-24 02:32:18 -0400

    feat(api): register DbContext, repositories and seed DB

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,20 +1,34 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using PruebaTecnica.DGII.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-// Repositories
-builder.Services.AddSingleton<PruebaTecnica.DGII.Interfaces.IContribuyenteRepository, PruebaTecnica.DGII.Repositories.InMemoryContribuyenteRepository>();
-builder.Services.AddSingleton<PruebaTecnica.DGII.Interfaces.IComprobanteRepository, PruebaTecnica.DGII.Repositories.InMemoryComprobanteRepository>();
+// Register EF repositories for production usage
+builder.Services.AddScoped<PruebaTecnica.DGII.Interfaces.IContribuyenteRepository, PruebaTecnica.DGII.Repositories.ContribuyenteRepository>();
+builder.Services.AddScoped<PruebaTecnica.DGII.Interfaces.IComprobanteRepository, PruebaTecnica.DGII.Repositories.ComprobanteRepository>();
 // Services
 builder.Services.AddScoped<PruebaTecnica.DGII.Services.ContribuyenteService>();
 builder.Services.AddScoped<PruebaTecnica.DGII.Services.ComprobanteService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// EF Core
+var connectionString = builder.Configuration.GetConnectionString("Default") ?? "Data Source=prueba_tecnica.db";
+builder.Services.AddDbContext<PruebaTecnicaContext>(options => options.UseSqlite(connectionString));
+
 var app = builder.Build();
+
+// Ensure DB created and seeded if using EF Core
+using (var scope = app.Services.CreateScope())
+{
+    var ctx = scope.ServiceProvider.GetRequiredService<PruebaTecnicaContext>();
+    ctx.Database.EnsureCreated();
+    ctx.EnsureSeedData();
+}
 
 if (app.Environment.IsDevelopment())
 {
@@ -28,4 +42,4 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+await app.RunAsync();
