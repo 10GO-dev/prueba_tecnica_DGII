b5fc1dcefa6d5f6961444aa23b6eae152f1fd532
10GO-dev <Diegoperalta0929@gmail.com>
2025-08-24 02:34:58 -0400

    fix(api): add missing using System.Linq and minor controller fixes

--- backend/Controllers/ContribuyentesController.cs
+++ backend/Controllers/ContribuyentesController.cs
@@ -3,6 +3,7 @@ using PruebaTecnica.DGII.Models;
 using PruebaTecnica.DGII.Services;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
+using System.Linq;
 using System;
 
 namespace PruebaTecnica.DGII.Controllers
