3b2befbc2e8db5303ff28f039b4a718311d9960d
10GO-dev <Diegoperalta0929@gmail.com>
2025-08-24 02:42:12 -0400

    chore(db): add initial EF Core migration

---
+++ backend/Data/Migrations/20250824064139_InitialCreate.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PruebaTecnica.DGII.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Comprobantes",
+                columns: table => new
+                {
+                    NCF = table.Column<string>(type: "TEXT", nullable: false),
+                    RncCedula = table.Column<string>(type: "TEXT", nullable: false),
+                    Monto = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comprobantes", x => x.NCF);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contribuyentes",
+                columns: table => new
+                {
+                    RncCedula = table.Column<string>(type: "TEXT", nullable: false),
+                    Nombre = table.Column<string>(type: "TEXT", nullable: false),
+                    Tipo = table.Column<string>(type: "TEXT", nullable: false),
+                    Estatus = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contribuyentes", x => x.RncCedula);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comprobantes");
+
+            migrationBuilder.DropTable(
+                name: "Contribuyentes");
+        }
+    }
+}
